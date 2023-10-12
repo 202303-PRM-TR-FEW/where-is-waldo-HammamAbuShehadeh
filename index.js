1039be1265a0cddd415ac29760ce6ce425b87654
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-10-12 18:57:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 const example = [
   ["A", "A", "A"],
   ["A", "A", "A"],
-  ["A", "B", "A"]];
+  ["A", "B", "A"],
+];
 
 const whereIsWaldo = (matrix) => {
-  
-}
+  let waldo = "";
+  for (let i = 0; i <= matrix.length - 1; i++) {
+    for (let j = 0; j <= matrix[i].length - 1; j++) {
+      waldo = matrix[i][0];
+      if (waldo === matrix[i][j]) {
+        continue;
+      } else {
+        waldo = matrix[i][j];
+        console.log([i + 1, j + 1]);
+        return [i + 1, j + 1];
+      }
+    }
+  }
+};
+
+whereIsWaldo(example);
+
+whereIsWaldo([
+  ["A", "A", "A"],
+  ["A", "A", "A"],
+  ["A", "B", "A"],
+]);
+// ➞ [3, 2]
+
+whereIsWaldo([
+  ["c", "c", "c", "c"],
+  ["c", "c", "c", "d"],
+]);
+// ➞ [2, 4]
+
+whereIsWaldo([
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+  ["P", "O", "O", "O"],
+  ["O", "O", "O", "O"],
+]);
+// ➞ [5, 1]
